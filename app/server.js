4841f4ac9cc081b4215b537584bb2b73b3a30d41
Per-Olov Jernberg <possan@possan.se>
2020-07-24 23:47:37 +0200

    Use port from env

--- app/server.js
+++ app/server.js
@@ -29,8 +29,9 @@ var udpPort = new osc.UDPPort({
 
 udpPort.open()
 
-http.listen(3000, function () {
-    console.log(`listening on *:3000`)
+const port = process.env.PORT || 3000
+http.listen(port, function () {
+    console.log(`listening on *:${port}`)
 })
 
 /* 
