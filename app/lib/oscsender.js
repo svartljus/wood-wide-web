976539d1cdfcf8bdd0cde89179c3457c91c4a0b2
Per-Olov Jernberg <possan@possan.se>
2020-07-28 19:03:01 +0200

    Do not send to broken devices

--- app/lib/oscsender.js
+++ app/lib/oscsender.js
@@ -4,10 +4,10 @@ export class OSCSender {
     constructor() {
         this.oscPort = null
         this.queue = []
+        this.brokenUntil = {};
     }
 
     start() {
-
         this.oscPort = new osc.UDPPort({
             localAddress: "0.0.0.0", // shouldn't matter here
             localPort: 5000, // not receiving, but here's a port anyway
@@ -16,6 +16,13 @@ export class OSCSender {
         })
 
         this.oscPort.open()
+
+        this.oscPort.on('error', e => {
+            // console.log('OSC error.', e.errno, e.code, e)
+            const key = `${e.address}:${e.port}`;
+            console.log(`OSC error, mark ${key} as broken.`)
+            this.brokenUntil[key] = Date.now() + 5000
+        })
 
         this.oscPort.on('ready', () => {
             console.log('OSC port open.')
@@ -55,11 +62,20 @@ export class OSCSender {
         const item = (this.queue.length > 0) ? this.queue.shift() : null
 
         if (item) {
-            try {
-                console.log(`Sending OSC to ${item.host}:${item.port} => ${JSON.stringify(item.message)}`)
-                this.oscPort.send(item.message, item.host, item.port)
-            } catch(e) {
-                console.log('Failed to send message', e)
+            // check if device is broken
+            const key = `${item.host}:${item.port}`;
+            const broken = this.brokenUntil[key]
+            if (broken && Date.now() < broken) {
+                // Don't send.
+                    // const key = `${item.host}:${item.port}`;
+                    // const broken = this.brokenUntil[key]
+            } else {
+                try {
+                    console.log(`Sending OSC to ${item.host}:${item.port} => ${JSON.stringify(item.message)}`)
+                    this.oscPort.send(item.message, item.host, item.port)
+                } catch(e) {
+                    console.log('Failed to send message', e)
+                }
             }
         }
 
