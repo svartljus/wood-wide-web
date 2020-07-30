49cbff991e541958086be3ea824bf6725b19fc65
Jonas Johansson <jns.johansson@gmail.com>
2020-07-30 17:32:57 +0200

    admin/client similar, devices separate, improved ui

---
+++ app/static/js/app.js
@@ -0,0 +1,369 @@
+var socket = io()
+
+let all_fixtures = null
+let selected_fixtures = []
+let preset_obj = {}
+
+function generatePreset() {
+    let ret = {}
+    ret._random = Math.floor(Math.random() * 100000)
+
+    CONTROLS.forEach(c => {
+        var el = document.getElementById(c.id)
+        ret[c.prop] = ~~el.value
+    })
+
+    return ret
+}
+
+function updatePresetCode() {
+    preset_obj = generatePreset()
+    const code = JSON.stringify(preset_obj)
+    document.getElementById('presetcode').value = code
+}
+
+function updateLabel(id) {
+    var ctrl = CONTROLS.find(c => c.id === id)
+    var el = document.getElementById(id)
+    el.oninput()
+    var el2 = document.querySelector('[data-label-for*="' + id + '"]')
+    var text = ctrl.formatter !== undefined ? ctrl.formatter(el.value) : `${el.value}`
+    el2.textContent = text
+}
+
+function updateColor(id) {
+    let value = document.getElementById(id).value
+    let layerId = id.substring(0, id.indexOf('-'))
+    let layerBtn = document.getElementById(layerId)
+    if (id.includes('red')) layerBtn.setAttribute('data-red', value)
+    if (id.includes('green')) layerBtn.setAttribute('data-green', value)
+    if (id.includes('blue')) layerBtn.setAttribute('data-blue', value)
+    if (id.includes('opacity')) layerBtn.setAttribute('data-alpha', value)
+    let c = {
+        r: Math.ceil(layerBtn.getAttribute('data-red') * 2) || 0,
+        g: Math.ceil(layerBtn.getAttribute('data-green') * 2) || 0,
+        b: Math.ceil(layerBtn.getAttribute('data-blue') * 2) || 0,
+        a: layerBtn.getAttribute('data-alpha') / 100 || 0
+    }
+    c.a = 1
+
+    document.querySelector(':root').style.setProperty(`--${layerId}-color`, `rgba(${c.r},${c.g},${c.b},${c.a})`)
+    // layerBtn.style.boxShadow = `inset 0 0 0 1000px rgba(${c.r},${c.g},${c.b},${c.a})`
+    // layerBtn.style.borderColor = `rgba(${c.r},${c.g},${c.b},${c.a})`
+    // document.getElementById('params').style.backgroundColor = `rgba(${c.r},${c.g},${c.b},${c.a}`
+    // layerBtn.style.backgroundColor = `rgba(${c.r},${c.g},${c.b},${c.a})`
+}
+
+function updateLabels() {
+    CONTROLS.forEach(c => {
+        updateLabel(c.id)
+    })
+}
+
+function updateColors() {
+    CONTROLS.forEach(c => {
+        if (c.id.match(/-red|-green|-blue|-opacity/)) {
+            console.log(c)
+            updateColor(c.id)
+        }
+    })
+}
+
+function updateInputsFromPreset() {
+    console.log('updateInputsFromPreset')
+
+    CONTROLS.forEach(c => {
+        var el = document.getElementById(c.id)
+        // console.log('el2', el)
+        el.value = preset_obj[c.prop]
+    })
+}
+
+function loadFromPresetObject(obj, sendupdates) {
+    if (obj) {
+        console.log('got object', obj)
+
+        Object.keys(obj).forEach(k => {
+            var ctrl = CONTROLS.find(c => (c.prop && c.prop === k) || (c.props && c.props.indexOf(k) !== -1))
+
+            preset_obj[k] = obj[k]
+
+            console.log('k', k, ctrl)
+            if (ctrl) {
+                if (sendupdates) {
+                    sendOSCUpdates(ctrl, obj)
+                }
+            }
+        })
+    }
+
+    updateInputsFromPreset()
+    updateLabels()
+    updateColors()
+}
+
+function loadFromPreset(presetCode, sendupdates) {
+    let obj = null
+    try {
+        obj = JSON.parse(presetCode)
+    } catch (e) {}
+
+    loadFromPresetObject(obj, sendupdates)
+}
+
+function updateSelectedFixtures(e) {
+    console.log('updateSelectedFixtures', e)
+
+    var idx = selected_fixtures.indexOf(e.target.id)
+    if (e.target.checked) {
+        if (idx === -1) {
+            selected_fixtures.push(e.target.id)
+        }
+    } else {
+        if (idx !== -1) {
+            selected_fixtures.splice(idx, 1)
+        }
+    }
+    console.log('new list of selected devices', selected_fixtures)
+}
+
+function updateFixtureLists() {
+    var el
+
+    el = document.getElementById('load-from-device')
+    if (el) {
+        // console.log('el', el)
+        el.options = new Array(10)
+        all_fixtures.forEach((f, i) => {
+            // console.log('f', f)
+            var o = new Option()
+            o.value = f.id
+            o.text = f.id + ' (' + f.displayname + ')'
+            el.options[i] = o
+        })
+    }
+
+    el = document.getElementById('devices')
+    if (el) {
+        // console.log('el', el)
+        el.innerHTML = ''
+        all_fixtures.forEach(f => {
+            // console.log('f', f)
+            var li = document.createElement('li')
+            var lab = document.createElement('label')
+            var inp = document.createElement('input')
+            var sp = document.createElement('span')
+            sp.textContent = ' ' + f.id + ' (' + f.displayname + ')'
+            inp.type = 'checkbox'
+            inp.id = f.id
+            inp.addEventListener('change', updateSelectedFixtures)
+            lab.appendChild(inp)
+            lab.appendChild(sp)
+            li.appendChild(lab)
+            el.appendChild(li)
+        })
+    }
+}
+
+function sendOSCUpdate(ctrl, fixture, broadcast, obj) {
+    if (ctrl.type === 'color') {
+        ctrl.props.forEach(prop => {
+            const msg = {
+                fixture,
+                broadcast,
+                prop: prop,
+                value: obj[prop]
+            }
+            console.log('send osc', msg)
+            socket.emit('set-fixture-property', msg)
+        })
+    } else {
+        const msg = {
+            fixture,
+            broadcast,
+            prop: ctrl.prop,
+            value: obj[ctrl.prop]
+        }
+        console.log('send osc', msg)
+        socket.emit('set-fixture-property', msg)
+    }
+}
+
+function sendOSCUpdates(ctrl, obj) {
+    var broadcast = document.getElementById('all-devices').checked
+    if (broadcast) {
+        sendOSCUpdate(ctrl, undefined, true, obj)
+    } else {
+        selected_fixtures.forEach(sf => {
+            sendOSCUpdate(ctrl, sf, false, obj)
+        })
+    }
+}
+
+function elementChanged(el) {
+    updateLabels()
+    updatePresetCode()
+    updateColors()
+
+    document.querySelector(':root').style.setProperty('--l1vh', window.innerHeight / 100 + 'px')
+
+    var ctrl = CONTROLS.find(c => c.id === el.id)
+    if (ctrl) {
+        sendOSCUpdates(ctrl, preset_obj)
+    }
+}
+
+socket.on('fixtures', fixtures => {
+    console.log('Got fixtures', fixtures)
+    all_fixtures = fixtures
+    updateFixtureLists()
+})
+
+function loadPresetFromCode() {
+    const code = document.getElementById('presetcode').value.trim()
+    loadFromPreset(code, true)
+}
+
+function sync() {
+    socket.emit('call-action', { action: 'sync' })
+}
+
+function sendAllProps() {
+    CONTROLS.forEach(ctrl => {
+        sendOSCUpdates(ctrl, preset_obj)
+    })
+}
+
+function loadFromSelectedDevice() {
+    const el = document.getElementById('load-from-device')
+    const selid = el.options[el.selectedIndex].value
+    if (selid) {
+        console.log('loadFromSelectedDevice', selid)
+        const fixture = all_fixtures.find(f => f.id === selid)
+        console.log('loadFromSelectedDevice', fixture)
+        if (fixture) {
+            loadFromPresetObject(fixture.props, false)
+        }
+    }
+}
+
+function loadFromSelectedDevice2() {
+    const el = document.getElementById('load-from-device')
+    const selid = el.options[el.selectedIndex].value
+    if (selid) {
+        console.log('loadFromSelectedDevice', selid)
+        const fixture = all_fixtures.find(f => f.id === selid)
+        console.log('loadFromSelectedDevice', fixture)
+        if (fixture) {
+            socket.emit('call-action', { action: 'fetch-config', fixture: fixture.id })
+        }
+    }
+}
+
+function setAdmin() {
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString)
+    const admin = urlParams.get('admin')
+    if (admin !== null) document.documentElement.id = 'admin'
+}
+
+function savePreset() {
+    var code = document.getElementById('presetcode').value
+    const name = prompt('Preset name')
+    if (name !== null) {
+        code = JSON.parse(code)
+        code.name = name
+        code = JSON.stringify(code)
+        console.log(code)
+        socket.emit('save-preset', code)
+    }
+}
+
+function loadPresets() {
+    const presets = []
+    const el = document.getElementById('all-presets')
+    el.addEventListener('change', () => {
+        var code = el.options[el.selectedIndex].value
+        loadFromPreset(code, true)
+        document.getElementById('presetcode').value = code
+    })
+
+    var o = new Option()
+    o.text = '-'
+    el.appendChild(o)
+    socket.emit('load-preset')
+    socket.on('preset-loaded', preset => {
+        presets.push(preset)
+        var o = new Option()
+        o.value = JSON.stringify(preset)
+        o.text = preset.name || 'preset'
+        el.appendChild(o)
+    })
+}
+
+window.addEventListener('load', () => {
+    console.log('loaded.')
+
+    setAdmin()
+    loadPresets()
+
+    CONTROLS.forEach(c => {
+        const inp = document.getElementById(c.id)
+        const par = inp.parentNode
+        const div = document.createElement('div')
+        const lab = document.createElement('label')
+        const span = document.createElement('span')
+        const range = c.range || [0, 100]
+        inp.min = range[0]
+        inp.max = range[1]
+        inp.value = 0.5 * (range[0] + range[1])
+        inp.classList.add('range-slider')
+        inp.oninput = () => {
+            // let a = inp.value
+            let a = (inp.value / inp.max) * 100
+            inp.style.background = `linear-gradient(to right,var(--slider-color),var(--slider-color) ${a}%,#eee ${a}%)`
+        }
+
+        inp.oninput()
+        lab.setAttribute('for', c.id)
+        lab.textContent = c.id.substr(c.id.lastIndexOf('-') + 1)
+        span.setAttribute('data-label-for', c.id)
+        span.textContent = 0
+        div.appendChild(lab)
+        div.appendChild(span)
+        par.appendChild(inp)
+        par.appendChild(div)
+
+        if (c.id) {
+            var el = document.getElementById(c.id)
+            if (el) {
+                // console.log('el', el, el.value)
+
+                el.addEventListener('change', e => {
+                    elementChanged(e.target)
+                })
+
+                el.addEventListener('mousemove', () => {
+                    updateLabels()
+                })
+            }
+        }
+    })
+
+    document.getElementById('load-from-preset').addEventListener('click', loadPresetFromCode)
+    // document.getElementById('sync').addEventListener('click', sync)
+    document.getElementById('send-all-props').addEventListener('click', sendAllProps)
+    document.getElementById('save-preset').addEventListener('click', savePreset)
+    document.getElementById('load-from-selected-device').addEventListener('click', loadFromSelectedDevice)
+    document.getElementById('load-from-selected-device2').addEventListener('click', loadFromSelectedDevice2)
+
+    loadFromPreset('{}', false)
+    updateLabels()
+    updateColors()
+    updatePresetCode()
+
+    window.onresize = function () {
+        document.querySelector(':root').style.setProperty('--vh', window.innerHeight / 100 + 'px')
+    }
+    window.onresize()
+})
---
+++ app/static/js/controls.js
@@ -0,0 +1,257 @@
+const speedMultiplierFormatter = value => `${value / 1000}x`
+
+const rpmFormatter = value => `${value / 1000} rpm`
+
+const multiplierFormatter = value => `${value} x`
+
+const percentFormatter = value => `${value}%`
+
+const percent1000Formatter = value => `${value / 1000}%`
+
+const CONTROLS = [
+    {
+        id: 'base-speed',
+        prop: 'basespeed',
+        formatter: rpmFormatter
+    },
+
+    {
+        id: 'opacity',
+        prop: 'opacity',
+        formatter: percentFormatter
+    },
+
+    {
+        id: 'layer1-red',
+        prop: 'layer1/red'
+    },
+
+    {
+        id: 'layer2-red',
+        prop: 'layer2/red'
+    },
+
+    {
+        id: 'layer3-red',
+        prop: 'layer3/red'
+    },
+
+    {
+        id: 'layer4-red',
+        prop: 'layer4/red'
+    },
+
+    {
+        id: 'layer1-green',
+        prop: 'layer1/green'
+    },
+
+    {
+        id: 'layer2-green',
+        prop: 'layer2/green'
+    },
+
+    {
+        id: 'layer3-green',
+        prop: 'layer3/green'
+    },
+
+    {
+        id: 'layer4-green',
+        prop: 'layer4/green'
+    },
+    {
+        id: 'layer1-blue',
+        prop: 'layer1/blue'
+    },
+
+    {
+        id: 'layer2-blue',
+        prop: 'layer2/blue'
+    },
+
+    {
+        id: 'layer3-blue',
+        prop: 'layer3/blue'
+    },
+
+    {
+        id: 'layer4-blue',
+        prop: 'layer4/blue'
+    },
+
+    {
+        id: 'layer1-speed',
+        prop: 'layer1/speed',
+        range: [1, 10000],
+        formatter: speedMultiplierFormatter
+    },
+    {
+        id: 'layer2-speed',
+        prop: 'layer2/speed',
+        range: [1, 10000],
+        formatter: speedMultiplierFormatter
+    },
+    {
+        id: 'layer3-speed',
+        prop: 'layer3/speed',
+        range: [1, 10000],
+        formatter: speedMultiplierFormatter
+    },
+    {
+        id: 'layer4-speed',
+        prop: 'layer4/speed',
+        range: [1, 10000],
+        formatter: speedMultiplierFormatter
+    },
+
+    {
+        id: 'layer1-opacity',
+        prop: 'layer1/opacity',
+        formatter: percentFormatter
+    },
+    {
+        id: 'layer2-opacity',
+        prop: 'layer2/opacity',
+        formatter: percentFormatter
+    },
+    {
+        id: 'layer3-opacity',
+        prop: 'layer3/opacity',
+        formatter: percentFormatter
+    },
+    {
+        id: 'layer4-opacity',
+        prop: 'layer4/opacity',
+        formatter: percentFormatter
+    },
+
+    {
+        id: 'layer1-blending',
+        prop: 'layer1/blending',
+        range: [0, 5]
+    },
+    {
+        id: 'layer2-blending',
+        prop: 'layer2/blending',
+        range: [0, 5]
+    },
+    {
+        id: 'layer3-blending',
+        prop: 'layer3/blending',
+        range: [0, 5]
+    },
+    {
+        id: 'layer4-blending',
+        prop: 'layer4/blending',
+        range: [0, 5]
+    },
+
+    {
+        id: 'layer1-offset',
+        prop: 'layer1/offset',
+        formatter: percentFormatter
+    },
+    {
+        id: 'layer2-offset',
+        prop: 'layer2/offset',
+        formatter: percentFormatter
+    },
+    {
+        id: 'layer3-offset',
+        prop: 'layer3/offset',
+        formatter: percentFormatter
+    },
+    {
+        id: 'layer4-offset',
+        prop: 'layer4/offset',
+        formatter: percentFormatter
+    },
+
+    {
+        id: 'layer1-repeat',
+        prop: 'layer1/repeat',
+        range: [1, 20],
+        formatter: multiplierFormatter
+    },
+    {
+        id: 'layer2-repeat',
+        prop: 'layer2/repeat',
+        range: [1, 20],
+        formatter: multiplierFormatter
+    },
+    {
+        id: 'layer3-repeat',
+        prop: 'layer3/repeat',
+        range: [1, 20],
+        formatter: multiplierFormatter
+    },
+    {
+        id: 'layer4-repeat',
+        prop: 'layer4/repeat',
+        range: [1, 20],
+        formatter: multiplierFormatter
+    },
+
+    {
+        id: 'layer1-size',
+        prop: 'layer1/size',
+        formatter: percentFormatter
+    },
+    {
+        id: 'layer2-size',
+        prop: 'layer2/size',
+        formatter: percentFormatter
+    },
+    {
+        id: 'layer3-size',
+        prop: 'layer3/size',
+        formatter: percentFormatter
+    },
+    {
+        id: 'layer4-size',
+        prop: 'layer4/size',
+        formatter: percentFormatter
+    },
+
+    {
+        id: 'layer1-feather1',
+        prop: 'layer1/feather1',
+        formatter: percentFormatter
+    },
+    {
+        id: 'layer2-feather1',
+        prop: 'layer2/feather1',
+        formatter: percentFormatter
+    },
+    {
+        id: 'layer3-feather1',
+        prop: 'layer3/feather1',
+        formatter: percentFormatter
+    },
+    {
+        id: 'layer4-feather1',
+        prop: 'layer4/feather1',
+        formatter: percentFormatter
+    },
+    {
+        id: 'layer1-feather2',
+        prop: 'layer1/feather2',
+        formatter: percentFormatter
+    },
+    {
+        id: 'layer2-feather2',
+        prop: 'layer2/feather2',
+        formatter: percentFormatter
+    },
+    {
+        id: 'layer3-feather2',
+        prop: 'layer3/feather2',
+        formatter: percentFormatter
+    },
+    {
+        id: 'layer4-feather2',
+        prop: 'layer4/feather2',
+        formatter: percentFormatter
+    }
+]
